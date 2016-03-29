1cf8a4af119848026d25947922a9cca6a199363d
nazmulapu <nazmul.apu.hasan@gmail.com>
2016-03-30 02:35:16 +0300

    Update Main.java
    Get set

--- InstanceMethod/src/naz/olivepress/Main.java
+++ InstanceMethod/src/naz/olivepress/Main.java
@@ -1,13 +1,37 @@
 package naz.olivepress;
 
+import java.util.ArrayList;
+
 import naz.olivepress.olives.Olive;
 import naz.olivepress.press.OlivePress;
 
 public class Main {
 	public static void main(String[] args) {
-		Olive[] olives = {new Olive(), new Olive(), new Olive()};
+		
+		ArrayList<Olive> olives = new ArrayList<Olive>();
+		Olive olive;
+		
+		olive = new Olive(2);
+		System.out.println(olive.name);
+		olives.add(olive);
+		
+		olive = new Olive(1);
+		System.out.println(olive.name);
+		olives.add(olive);
+		
+		olive = new Olive(2);
+		System.out.println(olive.name);
+		olives.add(olive);
+		
 		OlivePress press = new OlivePress();
 		press.getOil(olives);
+		
+		System.out.println("You got " + press.getTotalOil() + " units of oil");
+		
+        press.getOil(olives);
+		
+		System.out.println("You got " + press.getTotalOil() + " units of oil");
 	}
 
 }
+
