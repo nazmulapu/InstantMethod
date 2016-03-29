57a27f3ee3a1349713ca06c180fde6aa6f3b4f85
nazmul <nazmul.apu.hasan@gmail.com>
2016-03-29 23:51:40 +0300

    code update

--- InstanceMethod/src/naz/olivepress/Main.java
+++ InstanceMethod/src/naz/olivepress/Main.java
@@ -1,8 +1,13 @@
 package naz.olivepress;
 
+import naz.olivepress.olives.Olive;
+import naz.olivepress.press.OlivePress;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		Olive[] olives = {new Olive(), new Olive(), new Olive()};
+		OlivePress press = new OlivePress();
+		press.getOil(olives);
 	}
 
 }
--- InstanceMethod/src/naz/olivepress/press/OlivePress.java
+++ InstanceMethod/src/naz/olivepress/press/OlivePress.java
@@ -1,5 +1,12 @@
 package naz.olivepress.press;
 
+import naz.olivepress.olives.Olive;
+
 public class OlivePress {
+	public void getOil(Olive[] olives){
+		for (Olive olive : olives){
+			olive.crush();
+		}
+	}
 
 }
