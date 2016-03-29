8a428cd03fe2d8d59f56e2dccf700733d412182d
nazmulapu <nazmul.apu.hasan@gmail.com>
2016-03-29 23:55:43 +0300

    Update Olive.java

--- InstanceMethod/src/naz/olivepress/olives/Olive.java
+++ InstanceMethod/src/naz/olivepress/olives/Olive.java
@@ -1,5 +1,14 @@
 package naz.olivepress.olives;
 
 public class Olive {
+	
+	public String name = "Kalamata";
+	public String flavor = "Grassy";
+	public long color = 0x00000;
+	public int oil = 3;
+	
+	public void crush() {
+		System.out.println("ouch!");
+	}
 
 }
