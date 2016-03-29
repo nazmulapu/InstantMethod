d063dd914302d8153d95dd726832291e5c52f884
nazmul <nazmul.apu.hasan@gmail.com>
2016-03-30 00:51:33 +0300

    Constructor Introduced

--- InstanceMethod/src/naz/olivepress/press/OlivePress.java
+++ InstanceMethod/src/naz/olivepress/press/OlivePress.java
@@ -1,12 +1,24 @@
 package naz.olivepress.press;
 
+import java.util.ArrayList;
+
 import naz.olivepress.olives.Olive;
 
 public class OlivePress {
-	public void getOil(Olive[] olives){
+	
+	public OlivePress(){
+		
+	}
+	
+	public void getOil(ArrayList<Olive> olives){
+		
+		int oil = 0;
+		
 		for (Olive olive : olives){
-			olive.crush();
+			oil += olive.crush();
 		}
+		
+		System.out.println("You got " + oil + " units of oil");
 	}
 
 }
