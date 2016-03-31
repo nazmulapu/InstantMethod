552c2a1612fb470e9bf05b05a4831251cfc1f498
nazmulapu <nazmul.apu.hasan@gmail.com>
2016-03-31 03:52:11 +0300

    Update Olive.java
    Use of get and set

--- InstanceMethod/src/naz/olivepress/olives/Olive.java
+++ InstanceMethod/src/naz/olivepress/olives/Olive.java
@@ -5,10 +5,27 @@ public class Olive {
 	public String name = "Kalamata";
 	public String flavor = "Grassy";
 	public long color = 0x00000;
-	public int oil = 3;
+	private int oil = 3;
 	
-	public void crush() {
+	public int getOil() {
+		return oil;
+	}
+
+	public void setOil(int oil) {
+		this.oil = oil;
+	}
+
+	public Olive () {
+		System.out.println("Constructor of " + this.name);
+	}
+	
+	public Olive(int oil) {
+		this.oil = oil;
+	}
+
+    public int crush() {
 		System.out.println("ouch!");
+		return oil;
 	}
 
 }
