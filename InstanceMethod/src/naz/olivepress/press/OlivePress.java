c41d9865f791d3c9b12e81d7929568e6118174c5
nazmulapu <nazmul.apu.hasan@gmail.com>
2016-03-31 03:56:50 +0300

    Update OlivePress.java
    Constructor and Get set applied

--- InstanceMethod/src/naz/olivepress/press/OlivePress.java
+++ InstanceMethod/src/naz/olivepress/press/OlivePress.java
@@ -1,5 +1,36 @@
 package naz.olivepress.press;
 
+import java.util.ArrayList;
+
+import naz.olivepress.olives.Olive;
+
 public class OlivePress {
+	
+	private int totalOil = 0;
+	
+	public int getTotalOil() {
+		return totalOil;
+	}
+
+	private void setTotalOil(int totalOil) {
+		this.totalOil += totalOil;
+	}
+
+	public OlivePress(){
+		
+	}
+	
+	public void getOil(ArrayList<Olive> olives){
+		
+		int oil = 0;
+		
+		for (Olive olive : olives){
+			oil += olive.crush();
+		}
+		
+		setTotalOil(oil);
+		
+		
+	}
 
 }
